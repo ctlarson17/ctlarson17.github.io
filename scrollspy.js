2efd35babc9dd61fea9b83dd37833c6b47763dfe
ctlarson17 <143360623+ctlarson17@users.noreply.github.com>
2023-10-08 21:32:52 -0600

    Tailwind rewrite
    Completely redid the site in tailwind to make it mobile responsive.

--- scrollspy.js
+++ scrollspy.js
@@ -4,18 +4,13 @@ $(document).ready(function() {
         var position = $(this).scrollTop();
 
         // Iterate through content sections
-        $('#content > section').each(function() {
-            var target = $(this).offset().top;
-
-            var id = $(this).attr('id'); // get the ID of the current section
-            var navLink = $('#scrollspy a[href="#' + id + '"]');
-
-            // If this section is in the viewport
-            if (position >= target - 100) {
+        $('section').each(function() {
+            var top = $(this).offset().top - 100;
+            var bottom = top + $(this).outerHeight();
+            if (position >= top && position <= bottom) {
                 $('#scrollspy a').removeClass('active');
-                navLink.addClass('active');
-            } else {
-                navLink.removeClass('active');
+                var id = $(this).attr('id');
+                $('#scrollspy a[href="#' + id + '"]').addClass('active');
             }
         });
     });
@@ -25,7 +20,45 @@ $(document).ready(function() {
         event.preventDefault();
         var target = $(this.hash);
         $('html, body').animate({
-            scrollTop: target.offset().top - 48 // Subtracting 40 for the top nav offset
+            scrollTop: target.offset().top - 64 // Subtracting 40 for the top nav offset
         }, 600);
     });
 });
+
+
+$(document).ready(function() {
+    var currentSection = 0; // start with 0, and increment on the first click
+
+    // Get all the sections in an array
+    const sections = $('body div section');
+
+    // Dynamically get the number of sections
+    const totalSections = sections.length;
+
+    // Log the detected count to the console for debugging
+    console.log("Total sections detected:", totalSections);
+
+    $('#arrow-up').click(function() {
+        currentSection--;
+        if (currentSection < 0) {
+            currentSection = totalSections - 1; // Loop back to the last section
+        }
+        scrollToSection(currentSection);
+    });
+
+    $('#arrow-down').click(function() {
+        currentSection++;
+        if (currentSection >= totalSections) {
+            currentSection = 0; // Loop back to the first section
+        }
+        scrollToSection(currentSection);
+    });
+
+    function scrollToSection(sectionIndex) {
+        $('html, body').animate({
+            scrollTop: $(sections[sectionIndex]).offset().top -64
+        }, 600);
+    }
+});
+
+
